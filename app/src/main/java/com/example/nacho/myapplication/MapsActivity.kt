e81e42364f4b6a6da2da02c16f217a388f201eb3
Ignacio Galan <ignacio.galan@live.u-tad.com>
2018-12-10 18:57:54 +0100

    consigo que me aparezca el place picker pero no que se mantenga...

--- app/src/main/java/com/example/nacho/myapplication/MapsActivity.kt
+++ app/src/main/java/com/example/nacho/myapplication/MapsActivity.kt
@@ -1,20 +1,23 @@
 package com.example.nacho.myapplication
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.app.Activity
-import android.content.Context
+
+import android.content.pm.PackageManager
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.gms.maps.*
 
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import android.location.LocationListener
+
 import android.location.LocationManager
-import android.content.Context.LOCATION_SERVICE
-import android.content.pm.PackageManager
 import android.support.design.widget.FloatingActionButton
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.view.View
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.places.ui.PlacePicker
 
 import com.google.android.gms.maps.CameraUpdateFactory
 
@@ -24,12 +27,15 @@ import com.google.android.gms.maps.CameraUpdateFactory
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-    private var locationManager: LocationManager? = null
-    var PLACE_PICKER_REQUEST = 1
-    // private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var currentLocation: Location
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     companion object {
-        const val code: Int = 200
+
+
+        const val PLACE_PICKER_REQUEST = 1
+        const val REQUEST_LOCATION_PERMISSION = 1
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,32 +45,35 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-
-        val fab = findViewById<FloatingActionButton>(R.id.fab)
-        fab.setOnClickListener {
-            val PLACE_PICKER_REQUEST = 1
-            val builder = PlacePicker.IntentBuilder()
-
-            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST)
-        }
-
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         val home = LatLng(40.647988, -3.981343)
 
-       mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+        mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
         mMap.addMarker(MarkerOptions().position(home).title("Marker in Home"))
         mMap.setMinZoomPreference(10F)
         mMap.setMaxZoomPreference(20F)
         mMap.moveCamera(CameraUpdateFactory.newLatLng(home))
         mMap.uiSettings.isZoomControlsEnabled = true
 
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            val permissions = arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            ActivityCompat.requestPermissions(this, permissions, MapsActivity.REQUEST_LOCATION_PERMISSION)
+        }
+        else {
+
+        }
     }
 
+    fun showPlacePicker(view: View){
+        val builder = PlacePicker.IntentBuilder()
 
-
+        startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST)
+    }
 
 
 
