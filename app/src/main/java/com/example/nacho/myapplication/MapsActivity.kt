a2ae04adc3d617d5d89dc110a02b49d42b3edbd7
Ignacio Galan <ignacio.galan@live.u-tad.com>
2018-12-17 11:51:33 +0100

    with loading animation

--- app/src/main/java/com/example/nacho/myapplication/MapsActivity.kt
+++ app/src/main/java/com/example/nacho/myapplication/MapsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.nacho.myapplication
 
 
+import android.app.Activity
 import android.content.pm.PackageManager
 import android.location.Location
 import android.support.v7.app.AppCompatActivity
@@ -20,8 +21,13 @@ import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.places.ui.PlacePicker
 
 import com.google.android.gms.maps.CameraUpdateFactory
-
-
+import android.widget.Toast
+import com.google.android.gms.location.places.Place
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_maps.*
+import android.animation.Animator
+import android.app.PendingIntent.getActivity
+import android.util.Log
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -46,26 +52,68 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+
+        animation_view.setAnimation("loader.json")
+        animation_view.playAnimation()
+        animation_view.speed
+
+        animation_view.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+                Log.e("Animation:", "start")
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                Log.e("Animation:", "end")
+
+
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+                Log.e("Animation:", "cancel")
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+                Log.e("Animation:", "repeat")
+               // animation_view.visibility = View.GONE
+                animation_view.cancelAnimation()
+            }
+        })
+
+
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         val home = LatLng(40.647988, -3.981343)
 
-        mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
-        mMap.addMarker(MarkerOptions().position(home).title("Marker in Home"))
-        mMap.setMinZoomPreference(10F)
-        mMap.setMaxZoomPreference(20F)
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(home))
-        mMap.uiSettings.isZoomControlsEnabled = true
+
 
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
         {
             val permissions = arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION)
             ActivityCompat.requestPermissions(this, permissions, MapsActivity.REQUEST_LOCATION_PERMISSION)
+            mMap.setMyLocationEnabled(true)
         }
         else {
+            mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+            mMap.addMarker(MarkerOptions().position(home).title("Marker in Home"))
+            mMap.setMinZoomPreference(10F)
+            mMap.setMaxZoomPreference(20F)
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(home))
+            mMap.uiSettings.isZoomControlsEnabled = true
+        }
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == Activity.RESULT_OK) {
+                val place = PlacePicker.getPlace(data!!, this)
+                val toastMsg = String.format("Place: %s", place.name)
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(place.latLng))
+                mMap.addMarker(MarkerOptions().position(place.latLng).title("New marker"))
+
+            }
         }
     }
 
